357780952983b9fd0f86d94db10a274e7e7a8e68
Yihan Wang <yehuaywang@gmail.com>
2024-10-30 17:54:13 -0400

    game selection menu were implemented and wirings are fixed, db connection for game inventory have not yet been implemented, currently uses dummy data under /src/data.js

--- snake/client/src/components/Navbar.jsx
+++ snake/client/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import '../styles/Navbar.css'
+import MountAllRouters from '../routers/MountAllRouters';
 
 function Navbar() {
   return (
@@ -10,8 +11,8 @@ function Navbar() {
             <Link className="snake-logo" to="/">SNAKE!</Link>
         </div>
         <div className="navbar-item-list">
-            <Link className="nav-item" to="/reaction-game">Reaction</Link>
-            <Link className="nav-item" to="/memory-game">Memory</Link>
+            <Link className="nav-item" to="/reaction-games">Reaction</Link>
+            <Link className="nav-item" to="/memory-games">Memory</Link>
             <Link className="nav-item" to="/about">About Us</Link>
             <Link className="nav-item" to="/login">Log in</Link>
         </div>
---
+++ snake/client/src/routers/GameListRouters.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import GamePage from '../pages/GamePage'
+import { reactionGameList, memoryGameList } from '../data/tempGameListDatabase.js'
+
+function GameListRouters() {
+    const reactionRouters = reactionGameList.listOfGames.map(game =>
+        <Route 
+            key={game.id}
+            path={"/" + game.name}
+            element={<GamePage game={game} />}
+        />
+    )
+    const memoryRouters = memoryGameList.listOfGames.map(game =>
+        <Route 
+            key={game.id}
+            path={"/" + game.name} 
+            element={<GamePage game={game} />}
+        />
+    )
+    
+    return (
+        <Routes>
+            {reactionRouters}
+            {memoryRouters}
+        </Routes>
+    )
+}
+
+export default GameListRouters
