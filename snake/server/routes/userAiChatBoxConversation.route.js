d8d20e572d142d84ef40eecaf4f3f28098030568
Drewry_Tran <83705165+Wei-cyber@users.noreply.github.com>
2024-11-01 21:18:29 -0400

    #3 config server.js and setup

--- snake/server/routes/userAiChatBoxConversation.route.js
+++ snake/server/routes/userAiChatBoxConversation.route.js
@@ -2,7 +2,7 @@ import express from "express"
 import { createConversationCollection, createConversation, generateContent, getNewestMessage } from "../controllers/userAiChatBoxConversation.controller.js"
 
 const router = express.Router()
-
+const AI_KEY=process.env.AI_KEY
 router.post("/createNewConversationCollection", createConversationCollection)
 router.post("/createConversation", createConversation)
 router.post("/generateContent", generateContent)
