21304900dd23277b1f553d9ba35f9ef89a96a49e
prathamyr <prathamyr@gmail.com>
2024-11-02 23:43:27 -0400

    Dark mode + navbar change
    - Changed navbar
    - added a toggle for dark/light mode
    - changed logo alignment

--- snake/client/src/components/Navbar.jsx
+++ snake/client/src/components/Navbar.jsx
@@ -1,25 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom'
 import '../styles/Navbar.css'
 import MountAllRouters from '../routers/MountAllRouters';
 
 function Navbar() {
-  return (
+    const [darkMode, setDarkMode] = useState(false);
 
-    <nav className="navbar">
-        <div className="logo-div">
-            <Link className="snake-logo" to="/">SNAKE!</Link>
-        </div>
-        <div className="navbar-item-list">
-            <Link className="nav-item" to="/reaction-games">Reaction</Link>
-            <Link className="nav-item" to="/memory-games">Memory</Link>
-            <Link className="nav-item" to="/about">About Us</Link>
-            <Link className="nav-item" to="/login">Log in</Link>
-            <Link className="nav-item" to="/chatbox">ChatBot</Link>
-        </div>
-    </nav>
-    
-  )
+    const toggleTheme = () => {
+        document.body.classList.toggle('dark-theme');
+        setDarkMode(!darkMode);
+    };
+
+  return (
+      <nav className="navbar">
+          <div className="logo-div">
+              <Link className="snake-logo" to="/">SNAKE!</Link>
+          </div>
+          <div className="navbar-item-list">
+              <Link className="nav-item" to="/reaction-games">Reaction</Link>
+              <Link className="nav-item" to="/memory-games">Memory</Link>
+              <Link className="nav-item" to="/about">About Us</Link>
+              <Link className="nav-item" to="/login">Log in</Link>
+              <Link className="nav-item" to="/chatbox">ChatBot</Link>
+          </div>
+          <button onClick={toggleTheme} className="theme-toggle-button">
+                {darkMode ? 'Light Mode' : 'Dark Mode'}
+            </button>
+      </nav>
+  );
 }
 
 export default Navbar
