ef9dc842c3bfb79c7d16087f36bce842dfaa7743
cangyao <yehuaywang@gmail.com>
2024-11-02 21:15:05 -0400

    #3 fixed bugs in chatbot and re-established database connection, api now fully works!

--- snake/client/src/components/Aichatbot/ChatContainer.jsx
+++ snake/client/src/components/Aichatbot/ChatContainer.jsx
@@ -5,62 +5,59 @@ import '../../styles/Chatbot.css'
 import Messages from './Messages'
 
 function ChatContainer() {
-    // const [messages, setMessages] = useState([])
-    // const [messageInput, setMessageInput] = useState('')
-    // const [isLoading, setIsLoading] = useState(false)
-    // const [isButtonDisabled, setIsButtonDisabled] = useState(true)
+    const [inputValue, setInputValue] = useState('Ask alice...')
+    const [messageInput, setMessageInput] = useState('default user input')
+    const [isLoading, setIsLoading] = useState(false)
+    const [isButtonDisabled, setIsButtonDisabled] = useState(false)
+    const [isUser, setIsUser] = useState(false)
 
-    // const handleSend = () => {
+    const handleSend = () => {
+        if (inputValue.trim() !== '') {
+            setIsUser(true)
+            setMessageInput(inputValue)
+            setIsLoading(true)
+            setIsButtonDisabled(true)
+        }
+    }
 
-    // }
+    const handleMessagesRender = () => {
+        setIsUser(false)
+        setIsLoading(false)
+        setIsButtonDisabled(false)
+    }
 
-    // const loadWelcomingMessage = () => {
-        
-    // }
 
-    // return (
-    //     <div className="chat-container">
-    //         <div className="chat-header">
-    //             <h1>Playground</h1>
-    //         </div>
-    //         <div className="chat-messages" id="chat-messages">
-    //             {
-    //                 messages.map((message) => {
-    //                     return (
-    //                         <div 
-    //                             key={message.uuid}
-    //                             className={`message ${message.isUser ? 'user-message' : 'llm-message'}`}
-    //                         >
-    //                             {message.content}
-    //                         </div>
-    //                     )
-    //                 })
-    //             }
-    //         </div>
-    //         { isLoading && (
-    //             <div className="loading" id="loading">
-    //                 Alice is typing...
-    //             </div>
-    //         )}
-    //         <div className="input-container">
-    //             <input
-    //                 id="message-input"
-    //                 type="text"
-    //                 placeholder="Message Alice..." 
-    //                 value={messageInput}
-    //                 onChange={(e) => setMessageInput(e.target.value)}
-    //                 required 
-    //             />
-    //             <button id="send-button" onClick={handleSend} disabled={isButtonDisabled}>
-    //                 <img src="right-arrow.png" alt="right-arrow" width="30" height="30" />
-    //             </button>
-    //         </div>
-    //     </div>
-    // )
     return (
-      <div>
-        <h1> old container to be deleted </h1>
-      </div>
+        <div className="chat-container">
+            <div className="chat-header">
+                <h1>Playground</h1>
+            </div>
+            <Messages 
+                isUser={isUser}
+                userInput={messageInput} 
+                userEmail="1234567@email.com" 
+                updateWindow={isLoading} 
+                onMessagesRendered={handleMessagesRender}
+                />
+            { isLoading && (
+                <div className="loading" id="loading">
+                    Alice is typing...
+                </div>
+            )}
+            <div className="input-container">
+                <input
+                    id="message-input"
+                    type="text"
+                    placeholder="Message Alice..." 
+                    value={inputValue}
+                    onChange={(e) => setInputValue(e.target.value)}
+                    required 
+                />
+                <button id="send-button" onClick={handleSend} disabled={isButtonDisabled}>
+                    <img src="right-arrow.png" alt="right-arrow" width="30" height="30" />
+                </button>
+            </div>
+        </div>
     )
 }
 
--- snake/server/routes/chat.route.js
+++ snake/server/routes/chat.route.js
@@ -1,11 +1,13 @@
 import express from "express"
-import { createChat, appendMessage, getLatestMessage } from "../controllers/chat.controller.js"
+import { generateResponse, createChat, appendMessage, getLatestMessage, getAllMessages } from "../controllers/chat.controller.js"
 const router = express.Router()
 
 const AI_KEY=process.env.AI_KEY
 
+router.post("/generate", generateResponse)
 router.post("/create", createChat)
 router.post("/append", appendMessage)
 router.post("/latest", getLatestMessage)
+router.post("/all", getAllMessages)
 
 export default router
