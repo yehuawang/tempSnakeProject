d3e6015446aab3af5aa7c217c4ef78e6b54e74c7
Aryan Kalra <aryan.kalra2001@gmail.com>
2024-11-08 19:24:00 -0500

    #4 implemented user image upload and made front end changes

--- snake/server/routes/user.route.js
+++ snake/server/routes/user.route.js
@@ -1,7 +1,11 @@
 import express from "express"
-import { createUser, deleteUser, getUser, updateUser, loginUser } from "../controllers/user.controller.js"
+import { getUserProfileImage, uploadProfileImage, createUser, deleteUser, getUser, updateUser, loginUser } from "../controllers/user.controller.js"
 import { protectRoute } from "../middlewares/protectRoute.jwt.js"
+import multer from "multer"
 
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage })
+const uploadMiddleware = upload.single('profile-image')
 const router = express.Router()
 
 router.post("/getUser", getUser)
@@ -15,5 +19,8 @@ router.put("/:id", protectRoute, updateUser)
 router.delete("/:id", protectRoute, deleteUser)
 
 router.post("/login", loginUser)
+
+router.post("/upload-profile-image", uploadMiddleware, uploadProfileImage)
+router.post("/get-profile-image", getUserProfileImage)
 
 export default router
