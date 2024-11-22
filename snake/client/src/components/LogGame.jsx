993802f1f23db81fd8a7451fa8efae17a288e919
Drewry_Tran <83705165+Wei-cyber@users.noreply.github.com>
2024-11-21 22:47:17 -0500

    #26 setting up wiring with project for sequence memory game, logged new game to game db

--- snake/client/src/components/LogGame.jsx
+++ snake/client/src/components/LogGame.jsx
@@ -3,56 +3,9 @@ import React, {useState, useEffect} from 'react'
 
 const games = [
     {
-        id: "R-1",
-        name: "Snake",
-        body: "snake",
-        description: "The classic Snake game",
-        category: "reaction",
-        instruction: "Use the W-A-S-D keys to move the snake, eat the food to grow, do not eat yourself or bump into walls."
-    },
-    {
-        id: "R-2",
-        name: "Aim Trainer",
-        body: "aim-trainer",
-        description: "coming soon",
-        category: "reaction",
-        instruction: "coming soon"
-    },
-    {
-        id: "R-3",
-        name: "Typin Test",
-        body: "typin-test",
-        description: "Speed up your testing skills",
-        category: "reaction",
-        instruction: "Timer will go on once you pressed any keys, so start typing straight away. Use TAB to restart the test."
-    },
-    {
-        id: "R-4",
-        name: "Reaction Test",
-        body: "reaction-test",
-        description: "See how fast your reaction time is",
-        category: "reaction",
-        instruction: "Wait for the screen to change color, then click on the screen immediately. Do not click to early."
-    },
-    {
-        id: "M-1",
-        name: "Emoji Memo",
-        body: "emoji-memo",
-        description: "Can your brain run in parallel?",
-        category: "memory",
-        instruction: "Remember the current image, select the previous image."
-    },
-    {
-        id: "M-2",
-        name: "Word Game",
-        body: "word-game",
-        description: "Are you lucky enough to guess the word?",
-        category: "memory",
-        instruction: "You need to guess one secret word, you have 5 chances, you will be notified if a letter is correctly placed, wrongly placed, or not involved in the word."
-    },{
-        id: "M-3",
-        name: "Flip Cards",
-        body: "flip-cards",
+        id: "M-4",
+        name: "Sequence Memory",
+        body: "sequence Memory",
         description: "coming soon",
         category: "memory",
         instruction: "coming soon"
---
+++ snake/client/src/components/SequenceMemory/SequenceMemory.jsx
@@ -0,0 +1,11 @@
+import React, {useState, useEffect} from 'react'
+
+function SequenceMemory() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SequenceMemory
